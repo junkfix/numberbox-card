860c0e098e677dee2d65a5b8fd25c254978bb7f0
htmltiger <htmltiger@gmail.com>
2020-10-17 21:57:55 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,55 @@
+# numberbox-card
+
+NumberBox for input sliders
+
+Inspired from simple thermostat
+
+## Installation
+
+Manually add numberbox-card.js
+to your `<config>/www/` folder and add the following to the `configuration.yaml` file:
+```yaml
+lovelace:
+  resources:
+    - url: /local/numberbox-card.js?v=1
+      type: module
+```
+
+_OR_ install using [HACS](https://hacs.xyz/) and add this (if in YAML mode):
+```yaml
+lovelace:
+  resources:
+    - url: /hacsfiles/lovelace-numberbox-card/numberbox-card.js
+      type: module
+```
+
+The above configuration can be managed directly in the Configuration -> Lovelace Dashboards -> Resources panel when not using YAML mode,
+or added by clicking the "Add to lovelace" button on the HACS dashboard after installing the plugin.
+
+
+## Configuration
+
+| Name | Type | Default | Description
+| ---- | ---- | ------- | -----------
+| type | string | **Required** | `custom:numberbox-card`
+| entity | string | **Required** | `input_number.my_slider`
+| name | string/bool | `friendly_name` | Override friendly name (set to `false` to hide)
+| icon | string/bool | `icon` | Override icon (set to `false` to hide)
+
+## Examples
+
+![numberbox-card](https://github.com/htmltiger/numberbox-card/raw/main/example.png)
+
+Basic configuration:
+```yaml
+- type: custom:numberbox-card
+  entity: input_number.my_slider
+```
+
+```yaml
+- type: custom:numberbox-card
+  entity: input_number.my_slider
+  name: My Title
+  icon: 'mdi:fire'
+```
+
---
+++ numberbox-card.js
@@ -0,0 +1,182 @@
+((LitElement) => {
+    console.info('NUMBERBOX_CARD 1.0');
+	const html = LitElement.prototype.html;
+	const css = LitElement.prototype.css;
+	class NumberBox extends LitElement {
+
+		static get properties() {
+			return {
+				_hass: {},
+				config: {},
+				stateObj: {},
+			}
+		}
+
+		static get styles() {
+			return css`:host{--st-default-spacing: 1px}
+			ha-card{
+				-webkit-font-smoothing:var(--paper-font-body1_-_-webkit-font-smoothing);
+				font-size:var(--paper-font-body1_-_font-size);
+				font-weight:var(--paper-font-body1_-_font-weight);
+				line-height:var(--paper-font-body1_-_line-height);
+				padding:calc(var(--st-spacing, var(--st-default-spacing)) * 4) 0}
+			.body{
+				display:grid;grid-auto-flow:column;grid-auto-columns:1fr;
+				place-items:center;padding:0 calc(var(--st-spacing, var(--st-default-spacing)) * 4);
+				padding-bottom:calc(var(--st-spacing, var(--st-default-spacing)) * 2)}
+			.main{display:flex;flex-direction:row;align-items:center;justify-content:center}
+			.cur-box{display:flex;align-items:center;justify-content:center;flex-direction:row-reverse}
+			.cur-num-box{display:flex;align-items:center}
+			.cur-num{
+				font-size:var(--st-font-size-xl, var(--paper-font-display1_-_font-size));
+				line-height:var(--st-font-size-xl, var(--paper-font-display1_-_font-size));
+				font-weight:normal;margin:0}
+			.nopad{padding:0px}
+			.grid {
+			  display: grid;
+			  grid-template-columns: repeat(2, auto);
+			  cursor: pointer;
+			}
+			.grid-content {
+			  display: grid;
+			  align-content: space-between;
+			  grid-row-gap: 6px;
+			}
+			.grid-left {
+			  text-align: left;
+			  font-size: var(--paper-font-body1_-_font-size);
+			  padding: 16px 0 16px 16px;
+			  cursor: pointer;
+			}
+			.grid-right {
+			  text-align: right;
+			  padding-right: 10px;
+			}
+			`;
+		}
+
+		render() {
+			if(!this.stateObj){return html`<ha-card>Missing:'${this.config.entity}'</ha-card>`;}
+			const vars={};
+			if( this.config.icon === undefined && this.stateObj.attributes.icon ){
+				vars['icon']=this.stateObj.attributes.icon;
+			}else{
+				if(this.config.icon){
+					vars['icon']=this.config.icon;
+				}else{
+					vars['icon']=false;
+				}
+			}
+			if( this.config.name === undefined && this.stateObj.attributes.friendly_name ){
+				vars['name']=this.stateObj.attributes.friendly_name;
+			}else{
+				if(this.config.name){
+					vars['name']=this.config.name;
+				}else{
+					vars['name']=null;
+				}
+			}
+			return this.renderMain(vars);
+		}
+		renderMain(vars) {
+			return html`
+			<ha-card>
+				${vars.icon ? html`<div class="grid">
+				<div class="grid-content grid-left" @click="${() => this.moreInfo('hass-more-info')}">
+					<div>
+					<ha-icon icon="${vars.icon}" style="margin-right:10px;color:var(--paper-item-icon-color);"></ha-icon>
+					${vars.name}
+					</div>
+				</div><div class="grid-content grid-right">${this.renderNum()}</div></div>` : this.renderNum() }
+			</ha-card>
+		`;
+		}
+		renderNum(){
+			return html`<section class="body">
+					<div class="main">
+						<div class="cur-box">
+						<ha-icon-button class="nopad" icon="mdi:plus"
+							@click="${() => this.setNumb(true)}">
+						</ha-icon-button>
+						<div class="cur-num-box" @click="${() => this.moreInfo('hass-more-info')}" >
+							<h3 class="cur-num" > ${this.niceNum()} </h3>
+						</div>
+						<ha-icon-button class="nopad" icon="mdi:minus"
+							@click="${() => this.setNumb(false)}">
+						</ha-icon-button>
+						</div>
+					</div>
+					</section>`;
+		}
+		getCardSize() {
+			return 1;
+		}
+
+		setConfig(config) {
+			if (!config.entity) throw new Error('Please define an entity.');
+			if (config.entity.split('.')[0] !== 'input_number') throw new Error('Please define a input_number entity.');
+			this.config = {
+				name: config.name,
+				entity: config.entity,
+				icon: config.icon,
+			};
+		}
+
+		set hass(hass) {
+			if (hass && this.config) {
+				this.stateObj = this.config.entity in hass.states ? hass.states[this.config.entity] : null;
+			}
+			this._hass = hass;
+		}
+
+		callService(service, data = {entity_id: this.stateObj.entity_id}) {
+			const [domain, name] = service.split('.');
+			this._hass.callService(domain, name, data);
+		}
+		
+		publishNum(val){
+			this._hass.callService("input_number", "set_value", { entity_id: this.stateObj.entity_id, value: val });
+		}
+		
+		setNumb(addd){
+			const minn=Number(this.stateObj.attributes.min);
+			const maxx=Number(this.stateObj.attributes.max);
+			const step=Number(this.stateObj.attributes.step);
+			const curr=Number(this.stateObj.state);
+			console.info(this.config);
+			if(addd){
+				const adval=(curr + step);
+				if( adval <=  maxx){
+					this.publishNum(adval);
+				}
+			}else{
+				const adval=(curr - step);
+				if( adval >= minn){
+					this.publishNum(adval);
+				}				
+			}
+		}
+		
+		niceNum(){
+			const stp=Number(this.stateObj.attributes.step);
+			if( Math.round(stp) != stp ){
+				return Number(this.stateObj.state).toFixed(1);
+			}else{
+				return Number(this.stateObj.state).toFixed(0);
+			}
+		}
+
+		moreInfo(type, options = {}) {
+			const event = new Event(type, {
+				bubbles: options.bubbles || true,
+				cancelable: options.cancelable || true,
+				composed: options.composed || true,
+			});
+			event.detail = {entityId: this.stateObj.entity_id};
+			this.dispatchEvent(event);
+		}
+
+	}
+
+	customElements.define('numberbox-card', NumberBox);
+})(window.LitElement || Object.getPrototypeOf(customElements.get("hui-masonry-view") || customElements.get("hui-view")));
