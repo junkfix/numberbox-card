58d03efed33acf0267e738c143e43d947fdf0c31
htmltiger <1429451+htmltiger@users.noreply.github.com>
2021-02-07 06:48:33 +0000

    added secondary info

--- numberbox-card.js
+++ numberbox-card.js
@@ -1,6 +1,6 @@
 ((LitElement) => {
 
-console.info('NUMBERBOX_CARD 2.2');
+console.info('NUMBERBOX_CARD 2.3');
 const html = LitElement.prototype.html;
 const css = LitElement.prototype.css;
 class NumberBox extends LitElement {
@@ -9,6 +9,114 @@ constructor() {
 	super();
 	this.bounce = false;
 	this.pending = false;
+}
+
+render() {
+	if(!this.stateObj){return html`<ha-card>Missing:'${this.config.entity}'</ha-card>`;}
+	if( this.config.name === undefined && this.stateObj.attributes.friendly_name ){
+		this.config.name=this.stateObj.attributes.friendly_name;
+	}
+	if( this.config.icon === undefined && this.stateObj.attributes.icon ){
+		this.config.icon=this.stateObj.attributes.icon;
+	}
+	if( this.config.unit === undefined && this.stateObj.attributes.unit_of_measurement ){
+		this.config.unit=this.stateObj.attributes.unit_of_measurement;
+	}
+	if(!this.config.icon_plus){this.config.icon_plus='mdi:plus';}
+	if(!this.config.icon_minus){this.config.icon_minus='mdi:minus';}
+	if( this.config.delay === undefined ){ this.config.delay=1000;}
+
+	return html`
+	<ha-card class="${(!this.config.border)?'noborder':''}">
+		${(this.config.icon || this.config.name) ? html`<div class="grid">
+		<div class="grid-content grid-left" @click="${() => this.moreInfo('hass-more-info')}">
+			${this.config.icon ? html`<state-badge .overrideIcon="${this.config.icon}"></state-badge>` : null }
+			<div class="info">
+				${this.config.name?this.config.name:''}
+				${this.secondaryInfo()}
+			</div>
+		</div><div class="grid-content grid-right">${this.renderNum()}</div></div>` : this.renderNum() }
+	</ha-card>
+`;
+}
+
+secondaryInfo(){
+	if(!this.config.secondary_info){return;}
+	const v=this.config.secondary_info.replace('-','_');
+	if(this.stateObj[v]){
+		const t = new Date(this.stateObj[v]);
+		return html`
+		<div class="secondary">
+		<ha-relative-time .datetime=${t} .hass=${this._hass} ></ha-relative-time>
+		</div>`;
+	}
+}
+
+renderNum(){
+	return html`
+	<section class="body">
+	<div class="main">
+		<div class="cur-box">
+		<ha-icon class="padl" icon="${this.config.icon_plus}" @click="${() => this.setNumb(1)}" >
+		</ha-icon>
+		<div class="cur-num-box" @click="${() => this.moreInfo('hass-more-info')}" >
+			<h3 class="cur-num ${(this.pending===false)? '':'upd'}" > ${this.niceNum()} </h3>
+		</div>
+		<ha-icon class="padr" icon="${this.config.icon_minus}" @click="${() => this.setNumb(0)}" >
+		</ha-icon>
+		</div>
+	</div>
+	</section>`;
+}
+
+setNumb(c){
+	let v=this.pending; const a=this.stateObj.attributes;
+	const step=Number(a.step);
+	if( v===false ){ v=Number(this.stateObj.state); v=isNaN(v)?a.min:v;}
+	const adval=c?(v + step):(v - step);
+	if( adval <=  Number(a.max) && adval >= Number(a.min)){
+		this.pending=(adval);
+		clearTimeout(this.bounce);
+		this.bounce = setTimeout(this.publishNum, this.config.delay, this);
+	}
+}
+
+publishNum(dhis){
+	const v=dhis.pending;
+	dhis.pending=false;
+	dhis._hass.callService(dhis.stateObj.entity_id.split('.')[0], "set_value", { entity_id: dhis.stateObj.entity_id, value: v });
+}
+
+niceNum(){
+	let fix=0; let v=this.pending;
+	if( v === false ){ v=Number(this.stateObj.state); }
+	const stp=Number(this.stateObj.attributes.step);
+	if( Math.round(stp) != stp ){ fix=stp.toString().split(".")[1].length || 1;}
+	fix = v.toFixed(fix);
+	const u=this.config.unit;
+	if( u=="time" ){
+		return html`${this.zeroFill(Math.floor(fix/3600), 2)}:${this.zeroFill(Math.floor(fix/60), 2)}:${this.zeroFill(fix%60, 2)}`
+	}
+	return u===false ? fix: html`${fix}<span class="cur-unit" >${u}</span>`;
+}
+
+zeroFill(number, width){
+	width -= number.toString().length;
+	if ( width > 0 ){
+		return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+	}
+	return number + "";
+}
+
+moreInfo(type, options = {}) {
+	const e = new Event(type, {
+		bubbles: options.bubbles || true,
+		cancelable: options.cancelable || true,
+		composed: options.composed || true,
+	});
+	e.detail = {entityId: this.stateObj.entity_id};
+	this.dispatchEvent(e);
+	return e;
 }
 
 static get properties() {
@@ -29,11 +137,13 @@ static get styles() {
 		font-weight:var(--paper-font-body1_-_font-weight);
 		line-height:var(--paper-font-body1_-_line-height);
 		padding:4px 0}
+	ha-relative-time{color:var(--secondary-text-color);}
+	state-badge{flex:0 0 40px;}
 	ha-card.noborder{padding:0 !important;margin:0 !important;
 		box-shadow:none !important;border:none !important}
 	.body{
 		display:grid;grid-auto-flow:column;grid-auto-columns:1fr;
-		place-items:center;padding:0 4px}
+		place-items:center}
 	.main{display:flex;flex-direction:row;align-items:center;justify-content:center}
 	.cur-box{display:flex;align-items:center;justify-content:center;flex-direction:row-reverse}
 	.cur-num-box{display:flex;align-items:center}
@@ -49,103 +159,31 @@ static get styles() {
 	  grid-template-columns: repeat(2, auto);
 	}
 	.grid-content {
-	  display: grid;
+	  display: grid; align-items: center;
 	}
 	.grid-left {
+	  cursor: pointer;
+	  flex-direction: row;
+	  display: flex;
+      overflow: hidden;
+	}
+	.info{
+	  margin-left: 16px;
+	  margin-right: 8px;
 	  text-align: left;
 	  font-size: var(--paper-font-body1_-_font-size);
-	  padding: 8px 0 8px 16px;
-	  cursor: pointer;
-	  overflow: hidden;
-	  text-overflow: ellipsis;
-	  word-break: keep-all;
-	  white-space: nowrap
+	  flex: 1 0 30%;
 	}
-	.noborder .grid-left{padding-left:8px !important}
+	.info, .info > * {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+	}
 	.grid-right .body{margin-left:auto}
 	.grid-right {
-	  text-align: right;
-	  padding-right: 10px;
+	  text-align: right
 	}
-	.noborder .grid-right{padding-right:0 !important}
 	`;
-}
-
-shouldUpdate(changedProps) {
-	if( changedProps.has('stateObj') || changedProps.has('pending') ){return true;}
-}
-
-  
-render() {
-	if(!this.stateObj){return html`<ha-card>Missing:'${this.config.entity}'</ha-card>`;}
-	const vars={};
-	vars['icon_plus']=this.config.icon_plus ? this.config.icon_plus : 'mdi:plus';
-	vars['icon_minus']=this.config.icon_minus ? this.config.icon_minus : 'mdi:minus';
-	if( !this.config.border){
-		vars['border']='noborder';
-	}else{vars['border']='';}
-	if( this.config.icon === undefined && this.stateObj.attributes.icon ){
-		vars['icon']=this.stateObj.attributes.icon;
-	}else{
-		if(this.config.icon){
-			vars['icon']=this.config.icon;
-		}else{
-			vars['icon']=false;
-		}
-	}
-	if( this.config.unit === undefined && this.stateObj.attributes.unit_of_measurement ){
-		vars['unit']=this.stateObj.attributes.unit_of_measurement;
-	}else{
-		if(this.config.unit){
-			vars['unit']=this.config.unit;
-		}else{
-			vars['unit']=false;
-		}
-	}
-	if( this.config.name === undefined && this.stateObj.attributes.friendly_name ){
-		vars['name']=this.stateObj.attributes.friendly_name;
-	}else{
-		if(this.config.name){
-			vars['name']=this.config.name;
-		}else{
-			vars['name']=null;
-		}
-	}
-
-	return this.renderMain(vars);
-}
-
-renderMain(vars) {
-	let d=false;
-	if(vars.icon || vars.name){d=true;}
-	return html`
-	<ha-card class="${vars.border}">
-		${d ? html`<div class="grid">
-		<div class="grid-content grid-left" @click="${() => this.moreInfo('hass-more-info')}">
-			<div>
-			${vars.icon ? html`<ha-icon icon="${vars.icon}" style="margin-right:20px;color:var(--paper-item-icon-color);"></ha-icon>` : null }
-			${vars.name}
-			</div>
-		</div><div class="grid-content grid-right">${this.renderNum(vars)}</div></div>` : this.renderNum(vars) }
-	</ha-card>
-`;
-}
-
-renderNum(vars){
-	return html`
-	<section class="body">
-	<div class="main">
-		<div class="cur-box">
-		<ha-icon class="padl" icon="${vars.icon_plus}" @click="${() => this.setNumb(1)}" >
-		</ha-icon>
-		<div class="cur-num-box" @click="${() => this.moreInfo('hass-more-info')}" >
-			<h3 class="cur-num ${(this.pending===false)? null:'upd'}" > ${this.niceNum(vars)} </h3>
-		</div>
-		<ha-icon class="padr" icon="${vars.icon_minus}" @click="${() => this.setNumb(0)}" >
-		</ha-icon>
-		</div>
-	</div>
-	</section>`;
 }
 
 getCardSize() {
@@ -166,6 +204,8 @@ setConfig(config) {
 		unit: config.unit,
 		icon_plus: config.icon_plus,
 		icon_minus: config.icon_minus,
+		delay: config.delay,
+		secondary_info: config.secondary_info,
 	};
 }
 
@@ -176,53 +216,8 @@ set hass(hass) {
 	this._hass = hass;
 }
 
-setNumb(c){
-	let v=this.pending;
-	const step=Number(this.stateObj.attributes.step);
-	if( v===false ){ v=Number(this.stateObj.state); }
-	const adval=c?(v + step):(v - step);
-	if( adval <=  Number(this.stateObj.attributes.max) && adval >= Number(this.stateObj.attributes.min)){
-		this.pending=(adval);
-		clearTimeout(this.bounce);
-		this.bounce = setTimeout(this.publishNum, 1000, this);
-	}
-}
-
-publishNum(dhis){
-	const v=dhis.pending;
-	dhis.pending=false;
-	dhis._hass.callService(dhis.stateObj.entity_id.split('.')[0], "set_value", { entity_id: dhis.stateObj.entity_id, value: v });
-}
-
-zeroFill(number, width){
-	width -= number.toString().length;
-	if ( width > 0 ){
-		return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
-	}
-	return number + "";
-}
-
-niceNum(vars){
-	let fix=0; let v=this.pending;
-	if( v === false ){ v=Number(this.stateObj.state); }
-	const stp=Number(this.stateObj.attributes.step);
-	if( Math.round(stp) != stp ){ fix=stp.toString().split(".")[1].length || 1;}
-	fix = v.toFixed(fix);
-	if( vars.unit=="time" ){
-		return html`${this.zeroFill(Math.floor(fix/3600), 2)}:${this.zeroFill(Math.floor(fix/60), 2)}:${this.zeroFill(fix%60, 2)}`
-	}
-	return vars.unit ? html`${fix}<span class="cur-unit" >${vars.unit}</span>` : fix ;
-}
-
-moreInfo(type, options = {}) {
-	const e = new Event(type, {
-		bubbles: options.bubbles || true,
-		cancelable: options.cancelable || true,
-		composed: options.composed || true,
-	});
-	e.detail = {entityId: this.stateObj.entity_id};
-	this.dispatchEvent(e);
-	return e;
+shouldUpdate(changedProps) {
+	if( changedProps.has('stateObj') || changedProps.has('pending') ){return true;}
 }
 
 static getConfigElement() {
@@ -291,13 +286,21 @@ render() {
 		allow-custom-entity
 	></ha-entity-picker>
 </div>
-<ha-formfield label="Show border?">
-	<ha-switch
-		.checked=${this._border}
-		.configValue="${'border'}"
-		@change=${this.updVal}
-	></ha-switch>
-</ha-formfield>
+<div class="side">
+	<paper-input
+		label="Secondary Info (Optional)"
+		.value="${this.config.secondary_info}"
+		.configValue="${'secondary_info'}"
+		@value-changed="${this.updVal}"
+	></paper-input>
+	<ha-formfield label="Show border?">
+		<ha-switch
+			.checked=${this._border}
+			.configValue="${'border'}"
+			@change=${this.updVal}
+		></ha-switch>
+	</ha-formfield>
+</div>
 <div class="side">
 	<paper-input
 		label="Name (Optional, false to hide)"
@@ -333,6 +336,12 @@ render() {
 		.configValue=${'unit'}
 		@value-changed=${this.updVal}
 	></ha-icon-input>
+	<ha-icon-input
+		label="Delay (Default [1000] ms)"
+		.value="${this.config.delay}"
+		.configValue=${'delay'}
+		@value-changed=${this.updVal}
+	></ha-icon-input>
 </div>
 `;
 }
@@ -346,7 +355,7 @@ updVal(v) {
 	}
 	if (target.configValue) {
 		if (target.value === '') {
-			delete this.config[target.configValue];
+			try{delete this.config[target.configValue];}catch(e){}
 		} else {
 		if (target.value === 'false') {target.value = false;}
 			this.config = {
