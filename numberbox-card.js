12a135e736d5284da04197ee38cfc12ffd99085c
htmltiger <1429451+htmltiger@users.noreply.github.com>
2023-02-23 21:59:20 +0000

    Added max_entity min_entity

--- numberbox-card.js
+++ numberbox-card.js
@@ -1,6 +1,6 @@
 ((LitElement) => {
 
-console.info('NUMBERBOX_CARD 4.4');
+console.info('NUMBERBOX_CARD 4.5');
 const html = LitElement.prototype.html;
 const css = LitElement.prototype.css;
 class NumberBox extends LitElement {
@@ -28,12 +28,15 @@ render() {
 	if( this.config.unit === undefined && this.stateObj.attributes.unit_of_measurement ){
 		this.config.unit=this.stateObj.attributes.unit_of_measurement;
 	}
-	if(this.config.min === undefined){ this.config.min=this.stateObj.attributes.min;}
-	if(isNaN(parseFloat(this.config.min))){this.config.min=0;}
-	if(this.config.max === undefined){ this.config.max=this.stateObj.attributes.max;}
-	if(isNaN(parseFloat(this.config.max))){this.config.max=9e9;}
-	if('step_entity' in this.config && this.config.step_entity in this._hass.states && !isNaN(parseFloat(this._hass.states[this.config.step_entity].state))) {this.config.step=this._hass.states[this.config.step_entity].state;}
-	if(this.config.step === undefined){ this.config.step=this.stateObj.attributes.step;}
+	const d={min:0,max:9e9,step:1};
+	for(const j of Object.keys(d)) {
+		const b=j+'_entity';
+		if(b in this.config && this.config[b] in this._hass.states && !isNaN(parseFloat(this._hass.states[this.config[b]].state))) {
+			const c=this._hass.states[this.config[b]]; this.config[j]=c.state; this.old.t[this.config[b]]=c.last_updated
+		}		
+		if(this.config[j] === undefined){ this.config[j]=this.stateObj.attributes[j];}
+		if(isNaN(parseFloat(this.config[j]))){this.config[j]=d[j];}
+	}
 
 	return html`
 	<ha-card class="${(!this.config.border)?'noborder':''}">
